9c7da50e8577235ecef1000b0ebfeb73d765550b
daniel <daniel.bazan@digitrack.com.br>
2021-12-13 15:49:58 -0300

    add api gateway in front of lambda fun

--- ts-cdk-workshop/lib/ts-cdk-workshop-stack.ts
+++ ts-cdk-workshop/lib/ts-cdk-workshop-stack.ts
@@ -1,17 +1,20 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 
 export class TsCdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
-    })
+        const hello = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
+        });
 
-  }
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: hello
+        });
+    }
 }
