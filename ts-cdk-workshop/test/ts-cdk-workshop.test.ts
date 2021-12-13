93c6227cddb8ca9d9b5ac84dea07d6f2951c4db0
daniel <daniel.bazan@digitrack.com.br>
2021-12-13 17:10:58 -0300

    add some test to cdk constructs

--- ts-cdk-workshop/test/ts-cdk-workshop.test.ts
+++ ts-cdk-workshop/test/ts-cdk-workshop.test.ts
@@ -1,17 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as TsCdkWorkshop from '../lib/ts-cdk-workshop-stack';
+import {HitCounter} from "../lib/hitcounter";
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {Capture, Template} from "aws-cdk-lib/assertions";
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new TsCdkWorkshop.TsCdkWorkshopStack(app, 'MyTestStack');
-  // THEN
+test('DynamoDB table created', () => {
+    const stack = new cdk.Stack();
 
-  const template = Template.fromStack(stack);
+    // when
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFuncion', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hello.handler',
+            code: lambda.Code.fromAsset('lambda')
+        })
+    });
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
+    // then
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::DynamoDB::Table', 1);
+});
+
+test('Lambda has env variables', () => {
+    const stack = new cdk.Stack();
+
+    // when
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hello.handler',
+            code: lambda.Code.fromAsset('lambda')
+        })
+    });
+
+    // then
+    const template = Template.fromStack(stack);
+    const envCapture = new Capture();
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Environment: envCapture,
+    });
+
+    expect(envCapture.asObject()).toEqual({
+        Variables: {
+            DOWNSTREAM_FUNCTION_NAME: {
+                Ref: 'TestFunction22AD90FC'
+            },
+            HITS_TABLE_NAME: {
+                Ref: 'MyTestConstructHits24A357F0'
+            }
+        }
+    })
+});
+
+test('DynamoDb Table with encryptation', () => {
+    const stack = new cdk.Stack();
+
+    // when
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hello.handler',
+            code: lambda.Code.fromAsset('lambda')
+        })
+    });
+
+    // then
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        SSESpecification: {SSEEnabled: true}
+    });
 });
