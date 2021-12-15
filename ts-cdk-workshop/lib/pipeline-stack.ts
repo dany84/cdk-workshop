5865b2de42d78607dfd7d5a1debd7c6a72554c35
daniel <daniel.bazan@digitrack.com.br>
2021-12-15 11:26:20 -0300

    adjusting CI

--- ts-cdk-workshop/lib/pipeline-stack.ts
+++ ts-cdk-workshop/lib/pipeline-stack.ts
@@ -12,8 +12,10 @@ export class WorkshopPipelineStack extends cdk.Stack {
                 input: CodePipelineSource.connection('dany84/cdk-workshop', 'main', {
                     connectionArn: 'arn:aws:codestar-connections:sa-east-1:383827279863:connection/2fe91f14-1847-4ee5-a8fd-c0e859a7b05a'
                 }),
+                primaryOutputDirectory: 'ts-cdk-workshop/cdk.out',
                 commands: [
-                    'cd ./ts-cdk-workshop && npm ci',
+                    'cd ts-cdk-workshop',
+                    'npm ci',
                     'npm run build',
                     'npx cdk synth'
                 ]
