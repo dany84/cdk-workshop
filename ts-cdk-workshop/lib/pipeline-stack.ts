135a2002a5bd620248bf3c422329b40b9a6f47f0
daniel <daniel.bazan@digitrack.com.br>
2021-12-15 11:39:51 -0300

    Add deploy stage to pipeline

--- ts-cdk-workshop/lib/pipeline-stack.ts
+++ ts-cdk-workshop/lib/pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from "constructs";
 import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
+import {WorkshopPipelineStage} from "./pipeline-stage";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +21,9 @@ export class WorkshopPipelineStack extends cdk.Stack {
                     'npx cdk synth'
                 ]
             })
-        })
+        });
+
+        const deploy = new WorkshopPipelineStage(this, 'Deploy');
+        const deployStage = pipeline.addStage(deploy);
     }
 }
